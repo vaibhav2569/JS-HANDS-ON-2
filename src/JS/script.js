0abb099fc677daad234b514d534c6a18faf511aa
vaibhav2569 <vaibhavkumar@cedcoss.com>
2023-03-14 12:23:40 +0530

    10 tasks done

---
+++ src/JS/script.js
@@ -0,0 +1,41 @@
+// 1.Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//2. Use Bracket Notation to Find the First Character in a String
+firstLetterOfLastName = lastName[0]; 
+
+// 3. Understand String Immutability
+let myStr = "Jello World";
+myStr = "Hello World";
+
+// 4.Use Bracket Notation to Find the Nth Character in a String
+const thirdLetterOfLastName = lastName[2];
+
+
+// 5.Use Bracket Notation to Find the Last Character in a String
+const lastLetterOfLastName = lastName[lastName.length -1];
+
+// 6.Use Bracket Notation to Find the Nth-to-Last Character in a String
+const secondToLastLetterOfLastName = lastName[lastName.length-2];
+
+// 7.Word Blanks
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+const wordBlanks = myNoun+" "+myAdjective+" "+myVerb+" "+" "+myAdverb ;
+
+// 8.Store Multiple Values in one Variable using JavaScript Arrays
+const myArray = ["hello",25];
+
+// 9.Nest one Array within Another Array
+const Array = [["Bulls", 23], ["White Sox", 45]];
+
+// 10.Access Array Data with Indexes
+const rray = [50, 60, 70];
+const myData=rray[0];
